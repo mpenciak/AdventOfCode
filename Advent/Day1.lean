26f97c524b1c6bf1d4adff9c15e95a8dc4636270
mpenciak <matej.penciak@gmail.com>
2022-12-01 18:22:15 -0500

    Clean up day 1

--- Advent/Day1.lean
+++ Advent/Day1.lean
@@ -2,10 +2,12 @@ import Advent.Util
 
 def day1 : IO Unit := do
   let inputLines ← getInputLines false 1
-  let parsedInput := inputLines.split' (· == "") |>.map (·.map (fun s => parseNum.run s))
-  let cleanedInput := parsedInput.map cleanExcept
-  let sortedCalories := cleanedInput.map (Array.sum ·) |>.qsort (· < ·)
-  let (top1, top2, top3) := (sortedCalories.back, sortedCalories.pop.back, sortedCalories.pop.pop.back)
+  let sortedCalories := inputLines |>.split' (· == "") 
+                                   |>.map (·.map (fun s => parseNum.run s))
+                                   |>.map cleanExcept
+                                   |>.map (·.sum)
+                                   |>.qsort (· ≥ ·)
+  let (top1, top2, top3) := (sortedCalories[0]!, sortedCalories[1]!, sortedCalories[2]!)  
   printAnswer 1 top1 (top1 + top2 + top3)
 
 #eval day1
