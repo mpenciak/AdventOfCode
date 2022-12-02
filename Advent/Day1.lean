64baa7fe4577a9cc0bf87a92c47971e6a982a0d5
mpenciak <matej.penciak@gmail.com>
2022-12-02 09:17:51 -0500

    clarify inputs to IO stuff

--- Advent/Day1.lean
+++ Advent/Day1.lean
@@ -1,13 +1,13 @@
 import Advent.Util
 
 def day1 : IO Unit := do
-  let inputLines ← getInputLines false 1
+  let inputLines ← getInputLines (test := false) 1
   let sortedCalories := inputLines |>.split' (· == "") 
                                    |>.map (·.map (fun s => parseNum.run s))
                                    |>.map cleanExcept
                                    |>.map (·.sum)
                                    |>.qsort (· ≥ ·)
   let (top1, top2, top3) := (sortedCalories[0]!, sortedCalories[1]!, sortedCalories[2]!)  
-  printAnswer 1 top1 (top1 + top2 + top3)
+  printAnswer (day := 1) top1 (top1 + top2 + top3)
 
 #eval day1
